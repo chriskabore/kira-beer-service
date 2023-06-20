1ed968175711b292acbd091faaeedc4a58c8754b
christian <chriskabor@gmail.com>
2023-06-20 23:06:02 +0000

    created initial project for kira beer service. closes #1

---
+++ kira-beer-service/README.md
@@ -0,0 +1,3 @@
+ Kira Beer Service
+
+spring boot microservice for kira beer service
---
+++ kira-beer-service/src/test/java/com/kirasoft/kirabeerservice/KiraBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirasoft.kirabeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiraBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
